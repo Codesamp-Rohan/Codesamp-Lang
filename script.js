59c57622f28f1b8e110041fcb949e0c92196569a
Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-03-19 19:51:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,3 +16,21 @@ function langpvisible3(){
 function langphidden3(){
     document.getElementById("lang_p3").style.visibility = "hidden";
 }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
---
+++ signup.js
@@ -0,0 +1,3 @@
+function signup(){
+    window.alert("Thanks for Signing Up");
+}
