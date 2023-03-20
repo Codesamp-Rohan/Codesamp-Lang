8d347961d459798612506c5ba704861fec86da3a
Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-03-20 20:06:15 +0530

    Add files via upload

---
+++ signin.js
@@ -0,0 +1,13 @@
+function signup(){
+    window.alert("Thanks for Signing In");
+}
+function light(){
+    document.getElementById("navbar").style.backgroundColor = "#d0d0d0";
+    document.getElementById("body").style.backgroundColor = "white";
+    document.getElementById("body").style.color = "black";
+    document.getElementById("input_mail").style.backgroundColor = "#ebebeb";
+    document.getElementById("input_pass").style.backgroundColor = "#ebebeb";
+    document.getElementById("input1").style.color = "black";
+    document.getElementById("inpyt2").style.color = "black";
+    document.getElementById("submit_btn").style.backgroundColor = "#c15241"
+}
