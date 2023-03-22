2a1e9bdf1d2f6dbdbe886270ecef51e604241b0d
Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-03-22 21:14:48 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-function langpvisible1(){
-    document.getElementById("lang_p1").style.visibility = "visible";
-}
-function langphidden1(){
-    document.getElementById("lang_p1").style.visibility = "hidden";
-}
-function langpvisible2(){
-    document.getElementById("lang_p2").style.visibility = "visible";
-}
-function langphidden2(){
-    document.getElementById("lang_p2").style.visibility = "hidden";
-}
-function langpvisible3(){
-    document.getElementById("lang_p3").style.visibility = "visible";
-}
-function langphidden3(){
-    document.getElementById("lang_p3").style.visibility = "hidden";
-}
-
-function show(){
-  document.getElementById("myDropdown").style.display = "flex";
-}
-function remove(){
-document.getElementById("myDropdown").style.display = "none";
-}
 
 function bright(){
     document.getElementById("navbar").style.backgroundColor = "#ebebeb";
