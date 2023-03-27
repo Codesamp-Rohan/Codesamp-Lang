82a7d8cef7f1b2a8ce62c9d059a0bc70ab0db8ed
Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-03-27 11:24:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-
+function image(){
+    document.getElementById("background_img").style.display = "none";
+    document.getElementById("background_img2").style.display = "block";
+}
 function bright(){
     document.getElementById("navbar").style.backgroundColor = "#ebebeb";
     document.getElementById("body").style.backgroundColor = "white";
@@ -14,4 +17,41 @@ function bright(){
     document.getElementById("right_arrow10").style.backgroundColor = "#d6d6d6";
     document.getElementById("right_arrow11").style.backgroundColor = "#d6d6d6";
     document.getElementById("right_arrow12").style.backgroundColor = "#d6d6d6";
+    document.getElementById("codesamp_career").style.color = "#d8c1e0"
+}
+function arrow_hover1(){
+    document.getElementById("right_arrow1").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover2(){
+    document.getElementById("right_arrow2").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover3(){
+    document.getElementById("right_arrow3").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover4(){
+    document.getElementById("right_arrow4").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover5(){
+    document.getElementById("right_arrow5").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover6(){
+    document.getElementById("right_arrow6").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover7(){
+    document.getElementById("right_arrow7").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover8(){
+    document.getElementById("right_arrow8").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover9(){
+    document.getElementById("right_arrow9").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover10(){
+    document.getElementById("right_arrow10").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover11(){
+    document.getElementById("right_arrow11").style.backgroundColor = "#8f58847a";
+}
+function arrow_hover12(){
+    document.getElementById("right_arrow12").style.backgroundColor = "#8f58847a";
 }
