0a9a21143a7cb1148bf0b2e17f07acae1617ed9b
Rohan <121369255+Codesamp-Rohan@users.noreply.github.com>
2023-03-27 10:44:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,16 @@
 function bright(){
     document.getElementById("navbar").style.backgroundColor = "#ebebeb";
     document.getElementById("body").style.backgroundColor = "white";
+    document.getElementById("right_arrow1").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow2").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow3").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow4").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow5").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow6").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow7").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow8").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow9").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow10").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow11").style.backgroundColor = "#d6d6d6";
+    document.getElementById("right_arrow12").style.backgroundColor = "#d6d6d6";
 }
